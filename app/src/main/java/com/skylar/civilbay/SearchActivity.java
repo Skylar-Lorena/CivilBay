f9aef5df0b3d66242bdd57473bd0066382aaec12
Lorenah <lorayne911@gmail.com>
2021-01-08 18:12:05 +0300

    added intents to pass data

--- app/src/main/java/com/skylar/civilbay/SearchActivity.java
+++ app/src/main/java/com/skylar/civilbay/SearchActivity.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
+    private TextView profileTextView;
+    private EditText userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        profileTextView = (TextView) findViewById(R.id.profileTextView);
+        userName = (EditText) findViewById(R.id.userName);
         Intent intent = getIntent();
-        String name = intent.getStringExtra("profile");
-
+        String profile = intent.getStringExtra("profile");
+        profileTextView.setText("Welcome: " + userName);
     }
 }
