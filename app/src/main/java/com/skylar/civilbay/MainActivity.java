3c57e27b1e9bc69e289766ef8d8515c4c6c666ff
Lorenah <lorayne911@gmail.com>
2021-01-08 12:06:38 +0300

    added new activity, implemented intents

--- app/src/main/java/com/skylar/civilbay/MainActivity.java
+++ app/src/main/java/com/skylar/civilbay/MainActivity.java
@@ -2,6 +2,7 @@ package com.skylar.civilbay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         joinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
                 Toast.makeText(MainActivity.this, "Welcome to Civil Bay!", Toast.LENGTH_LONG).show();
             }
         });
