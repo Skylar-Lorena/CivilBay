fae1ccae48f0259af6a192f4246efbd7c8d79196
Lorenah <lorayne911@gmail.com>
2021-01-08 11:58:41 +0300

    added toast and onclick listener

--- app/src/main/java/com/skylar/civilbay/MainActivity.java
+++ app/src/main/java/com/skylar/civilbay/MainActivity.java
@@ -3,12 +3,25 @@ package com.skylar.civilbay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUpButton =  (Button) findViewById((R.id.signUpButton));
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Welcome to Civil Bay!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
